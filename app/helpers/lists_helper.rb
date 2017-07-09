41c4e9d594214753120374fe65db9f390e7c99a6
goldenhub777 <sg300c@gmail.com>
2017-07-09 15:19:46 -0400

    fixed deprecation issue with moment() calls in lists.js, and disabled verbose logging in rspec tests

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,2 +1,10 @@
 module ListsHelper
+
+  def time_to_s(date_obj)
+    begin
+      date_obj.strftime("%Y-%m-%dT%H:%M+00:00")
+    rescue DateTime::ArgumentError, DateTime::NoMethodError, DateTime::TypeError => message
+      nil
+    end
+  end
 end
--- app/javascript/packs/lists.js
+++ app/javascript/packs/lists.js
@@ -14,6 +14,7 @@ $(document).on("turbolinks:load", () => {
 
   $("#list-date-picker").datetimepicker({
     format: 'MM/DD/YYYY hh:mm A',
+    defaultDate: false,
     useCurrent: false
   });
 
