61b55e3ba5e2d7e352db65422dacceab1b651f3f
goldenhub777 <sg300c@gmail.com>
2017-07-12 23:38:40 -0400

    edited User model and views, added name and profile_picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,16 @@ class User < ApplicationRecord
 
   validates :provider, presence: true, allow_blank: true
   validates :uid, presence: true, allow_blank: true
-  
+  validate :password_complexity
+  validates :name, presence: true
+
   #handled returned OAuth data to create a user
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      # user.name = auth.info.name   # assuming the user model has a name
-      # user.image = auth.info.image # assuming the user model has an image
+      user.name = auth.info.name   # assuming the user model has a name
+      user.profile_picture = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!
@@ -25,8 +27,15 @@ class User < ApplicationRecord
   def self.new_with_session(params, session)
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.name = data["name"] if user.name.blank?
         user.email = data["email"] if user.email.blank?
       end
+    end
+  end
+
+  def password_complexity
+    if provider.present? && password.present? and not password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d). /)
+      errors.add :password, "must include at least one lowercase letter, one uppercase letter, and one digit"
     end
   end
 end
---
+++ db/migrate/20170713025009_add_columns_name_and_profile_picture_to_users.rb
@@ -0,0 +1,6 @@
+class AddColumnsNameAndProfilePictureToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :name, :string, null: false
+    add_column :users, :profile_picture, :text
+  end
+end
